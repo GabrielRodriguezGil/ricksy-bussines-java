f4ccffec28d99b9970294d6885e18b5cd7db8b97
GabrielRodriguezGil <gabrielrodgil088@gmail.com>
2026-05-25 14:29:51 +0200

    feat(proyect): :sparkles: Commit Apresurado

---
+++ app/src/main/java/edu/teamrocket/dispatcher/UfosPark.java
@@ -0,0 +1,57 @@
+package edu.teamrocket.dispatcher;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import edu.teamrocket.paymethod.PaymentMethod;
+
+public class UfosPark implements GuestDispatcher {
+
+    private double fee;
+    private Map<String, String> flota = new HashMap<>();
+
+    public UfosPark() {
+    }
+
+    public void add(String ufoID) {
+        flota.putIfAbsent(ufoID, null);
+    }
+
+    @Override
+    public void dispatch(PaymentMethod card) {
+
+        String ufo = null;
+
+        if (!flota.containsValue(card.number())) {
+            ufo = flota.entrySet().stream().filter(x -> x.getValue() == null).map(Map.Entry::getKey).findFirst()
+                    .orElse(null);
+        }
+        if (ufo != null && card.pay(fee)) {
+            flota.put(ufo, card.number());
+        }
+    }
+
+    public String getUfoOf(String ownerCard) {
+        return flota.entrySet().stream().filter(x -> x.getValue().equals(ownerCard)).map(Entry::getKey)
+                .findFirst().orElse(null);
+    }
+
+    boolean containsCard(String cardId) {
+        return flota.containsValue(cardId);
+    }
+
+    public Collection<String> cardNumbers() {
+        return flota.keySet();
+    }
+
+    @Override
+    public String toString() {
+        String[] ufosID = this.flota.keySet().toArray(new String[flota.size()]);
+        Arrays.sort(ufosID);
+        return List.of(ufosID).toString();
+    }
+}
